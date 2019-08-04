fc3d0e8294b19947cf90dbaf04ca3d981c414836
WhiteCollar818 <49412408+WhiteCollar818@users.noreply.github.com>
2019-08-05 02:57:27 +0300

    Create index.php

---
+++ homeworks/2.3-dates-and-sessions/2.3.1-your-tube/index.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * Функция получает текущее количество просмотров на видео
+ *
+ * @return int
+ */
+function getViews(): int
+{
+    $views = include 'views.php';
+    return (int)$views;
+}
+/**
+ * Функция увеличивает количество просмотров на 1
+ *
+ * @param int $views
+ */
+function incrementViews(int $views)
+{
+    $views++;
+    $data = "<?php \r\nreturn {$views};";
+    file_put_contents('views.php', $data);
+}
+/**
+ * Функция проверяет, нужно ли увеличивать число просмотров
+ *
+ * @return bool
+ */
+function shouldBeIncremented(): bool
+{
+    $startTime = time();
+    $timestamp = isset($_COOKIE['time']) ? $_COOKIE['time'] : 0;
+    setcookie('time', $startTime);
+    return (($startTime - $timestamp) >= 300); 
+}
+if (shouldBeIncremented()){
+    incrementViews(getViews());
+}
+//
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Your Tube</title>
+</head>
+<header style="width:100%; border-bottom: 1px solid black">
+    <b>YOUR TUBE</b>
+</header>
+<body>
+<div style="width: 69%; border-right: 1px solid black; display: inline-block">
+    <div style="text-align: center; border: 1px solid black; background-color: black; color: white; height: 39.375%">
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        Очень интересное видео
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+    </div>
+    <div style="margin-top: 2px; border-top: 1px solid black;">
+        <b>Просмотров: <?= getViews() ?> </b>
+    </div>
+</div>
+<div style="width: 29%; display: inline-block; margin-bottom: 100%">
+    <div style="text-align: center; border: 1px solid black; background-color: black; color: white; height: 39.375%; margin:1px">
+        <br>
+        <br>
+        Очень интересное видео
+        <br>
+        <br>
+    </div>
+    <div style="text-align: center; border: 1px solid black; background-color: black; color: white; height: 39.375%; margin:1px">
+        <br>
+        <br>
+        Очень интересное видео
+        <br>
+        <br>
+    </div>
+    <div style="text-align: center; border: 1px solid black; background-color: black; color: white; height: 39.375%; margin:1px">
+        <br>
+        <br>
+        Очень интересное видео
+        <br>
+        <br>
+    </div>
+    <div style="text-align: center; border: 1px solid black; background-color: black; color: white; height: 39.375%; margin:1px">
+        <br>
+        <br>
+        Очень интересное видео
+        <br>
+        <br>
+    </div>
+
+</div>
+
+</body>
+</html>
+© 2019 GitHub, Inc.
